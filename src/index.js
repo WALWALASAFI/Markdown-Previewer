69f9128effff1b194b3166fe9e0652bf7a2be6b7
WALWALASAFI <walwalasafi99@gmail.com>
2024-08-26 13:26:10 +0430

    solve the errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+/* eslint-enable no-unused-vars */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>, // Trailing comma added
 );
